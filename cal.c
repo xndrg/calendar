7846191ddb733517eae6e7d7432230ddfbdc70c7
xndrg <galay196@gmail.com>
2024-10-09 01:20:17 +0300

    Fix tabs

--- cal.c
+++ cal.c
@@ -34,9 +34,9 @@ typedef struct {
 bool is_leap_year(int year)
 {
     if((year % 4 == 0 && year % 100 != 0 ) || (year % 400 == 0))
-	return true;
+        return true;
     else
-	return false;
+        return false;
 }
 
 Month get_month(int year, int month)
@@ -44,11 +44,11 @@ Month get_month(int year, int month)
     switch (month) {
     case 0:  return (Month) {(String) {"January",   7}, 31};
     case 1: {
-	int days;
+        int days;
 
-	if (is_leap_year(year)) days = 29; else days = 28;
-	
-	return (Month) {(String) {"February",  8}, days};
+        if (is_leap_year(year)) days = 29; else days = 28;
+
+        return (Month) {(String) {"February",  8}, days};
     }
     case 2:  return (Month) {(String) {"March",     5}, 31};
     case 3:  return (Month) {(String) {"April",     5}, 30};
@@ -69,12 +69,12 @@ Gaps get_gaps(String month) {
     int left_gap, right_gap;
 
     if (gap % 2 == 0) {
-	left_gap = right_gap = gap / 2;
+        left_gap = right_gap = gap / 2;
     } else {
-	right_gap = gap / 2;
-	left_gap = right_gap + 1;
+        right_gap = gap / 2;
+        left_gap = right_gap + 1;
     }
-    
+
     return (Gaps) {left_gap, right_gap};
 }
 
@@ -82,12 +82,12 @@ Date get_current_date()
 {
     time_t t = time(NULL);
     struct tm *date_time = localtime(&t);
-    
+
     int   year        = date_time->tm_year + 1900;
     Month month       = get_month(year, date_time->tm_mon);
     int   day         = date_time->tm_mday;
     int   day_of_week = date_time->tm_wday;
-    
+
     return (Date) {day, month, year, day_of_week};
 }
 
@@ -97,11 +97,11 @@ void print_calendar(Date date)
     Gaps gaps = get_gaps(date.month.name);
 
     printf("%0*c%s %d%0*c\n",
-	   gaps.left_gap,  ' ',
-	   date.month.name.contents,
-	   date.year,
-	   gaps.right_gap, ' '); 
-    
+            gaps.left_gap,  ' ',
+            date.month.name.contents,
+            date.year,
+            gaps.right_gap, ' '); 
+
     printf("%s\n", header);
 
     // Maybe a bit strange way to find day of week of first day of given month
@@ -109,20 +109,20 @@ void print_calendar(Date date)
     int day_of_month = (date.day_of_week - date.day) + (date.day / 7) * 7 + 1;
 
     for (int i = 0; i < day_of_month; ++i)
-	printf("%2c ", ' ');
-    
-    for (int i = 1; i <= date.month.days; ++i) {
-	if (i == date.day) {
-	    // Using ANSI escape codes to change background color of current day
-	    printf("\033[30;107m%2d\033[0m ", i);
-	} else
-	    printf("%2d ", i);
+        printf("%2c ", ' ');
 
-	if (day_of_month == 6) {
-	    day_of_month = 0;
-	    printf("\n");
-	} else
-	    day_of_month++;
+    for (int i = 1; i <= date.month.days; ++i) {
+        if (i == date.day) {
+            // Using ANSI escape codes to change background color of current day
+            printf("\033[30;107m%2d\033[0m ", i);
+        } else
+            printf("%2d ", i);
+
+        if (day_of_month == 6) {
+            day_of_month = 0;
+            printf("\n");
+        } else
+            day_of_month++;
     }
 
     printf("\n");
@@ -132,6 +132,6 @@ int main(void)
 {
     Date current_date = get_current_date();
     print_calendar(current_date);
-    
+
     return 0;
 }
